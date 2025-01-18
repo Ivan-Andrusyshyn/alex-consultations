7ac81f05c7e054bb33d580bcccdfff4bbc898250
Ivan-Andrusyshyn <ivan.andrusyshyn@icloud.com>
2025-01-18 15:03:34 +0200

    initial app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,47 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./pages/home/home.component').then((m) => m.HomeComponent),
+  },
+  {
+    path: 'courses',
+    loadComponent: () =>
+      import('./pages/courses/courses.component').then(
+        (m) => m.CoursesComponent
+      ),
+  },
+  {
+    path: 'tests',
+    loadComponent: () =>
+      import('./pages/tests/tests.component').then((m) => m.TestsComponent),
+  },
+  {
+    path: 'consultations',
+    loadComponent: () =>
+      import('./pages/consultations/consultations.component').then(
+        (m) => m.ConsultationsComponent
+      ),
+  },
+  {
+    path: 'about',
+    loadComponent: () =>
+      import('./pages/about/about.component').then((m) => m.AboutComponent),
+  },
+  {
+    path: 'contact',
+    loadComponent: () =>
+      import('./pages/contacts/contacts.component').then(
+        (m) => m.ContactsComponent
+      ),
+  },
+  {
+    path: '**',
+    loadComponent: () =>
+      import('./pages/not-found/not-found.component').then(
+        (m) => m.NotFoundComponent
+      ),
+  },
+];
---
+++ src/app/pages/contacts/contacts.component.ts
@@ -0,0 +1,27 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-contacts',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './contacts.component.html',
+  styleUrl: './contacts.component.scss',
+})
+export class ContactsComponent implements OnInit {
+  private readonly fb = inject(FormBuilder);
+
+  contactForm!: FormGroup;
+
+  ngOnInit(): void {
+    this.contactForm = this.fb.group({
+      name: [''],
+      email: [''],
+      message: [''],
+    });
+  }
+
+  onSubmit() {
+    alert('Спасибо за ваше сообщение! Мы свяжемся с вами в ближайшее время.');
+  }
+}
