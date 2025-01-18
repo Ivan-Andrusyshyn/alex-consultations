0cad08d3cf746e8eee2af19570632e2d5495f63a
Ivan-Andrusyshyn <ivan.andrusyshyn@icloud.com>
2025-01-18 17:07:01 +0200

    add dialog-model in consultation section

--- src/app/pages/consultations/consultations.component.ts
+++ src/app/pages/consultations/consultations.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, inject, model, signal } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+
+import { ModalComponent } from '../../components/modal/modal.component';
 
 @Component({
   selector: 'app-consultations',
   standalone: true,
-  imports: [RouterLink],
+  imports: [MatFormFieldModule, MatInputModule, MatButtonModule],
   templateUrl: './consultations.component.html',
   styleUrl: './consultations.component.scss',
 })
-export class ConsultationsComponent {}
+export class ConsultationsComponent {
+  readonly dialog = inject(MatDialog);
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(ModalComponent, {
+      data: { name: '' },
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log('The dialog was closed');
+      if (result !== undefined) {
+        console.log(result);
+      }
+    });
+  }
+}
