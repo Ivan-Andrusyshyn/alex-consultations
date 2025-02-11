9572a58c2eb6eaddef47f2ea1677e317abaa7db0
Ivan-Andrusyshyn <ivan.andrusyshyn@icloud.com>
2025-02-11 12:10:23 +0200

    cors

--- server/build/utils/cors.js
+++ server/build/utils/cors.js
@@ -1,22 +1,21 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 const cors_1 = __importDefault(require("cors"));
-const corsOptions = () => (0, cors_1.default)({
+const corsOptions = () =>
+  (0, cors_1.default)({
     origin: [
-        'http://localhost:4200',
-<<<<<<< HEAD
-        'http://www.vidchuttia.com.ua/',
-=======
-        'http://www.vidchuttia.com.ua',
-        'http://vidchuttia.com.ua',
->>>>>>> 3e2d7f7de8ce92a6541a91390912a05731924b54
-        'https://alex-consultations.vercel.app',
+      "http://localhost:4200",
+      "http://www.vidchuttia.com.ua",
+      "http://vidchuttia.com.ua",
+      "https://alex-consultations.vercel.app",
     ],
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
-});
+  });
 exports.default = corsOptions;
