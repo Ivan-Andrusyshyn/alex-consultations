7b2c64d2c34b201eb435161ff52a97c9d5847ab6
Ivan-Andrusyshyn <ivan.andrusyshyn@icloud.com>
2025-02-16 13:22:27 +0200

    add expansion panel on indicator

--- src/app/components/test/traumatic-sensitivity/traumatic-indicator/traumatic-indicator.component.ts
+++ src/app/components/test/traumatic-sensitivity/traumatic-indicator/traumatic-indicator.component.ts
@@ -5,15 +5,18 @@ import {
   inject,
   Input,
   OnInit,
+  signal,
 } from '@angular/core';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 import { TraumaticSensitivityService } from '../../../../shared/services/traumatic-sensitivity.service';
 import { TestResult } from '../../../../shared/types/traumatic-sensitivity';
+import { SensitivityCategory, sensitivityTest } from './sensitivity-category';
 
 @Component({
   selector: 'app-traumatic-indicator',
   standalone: true,
-  imports: [NgFor, NgIf, NgClass],
+  imports: [NgFor, NgIf, NgClass, MatExpansionModule],
   templateUrl: './traumatic-indicator.component.html',
   styleUrl: './traumatic-indicator.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -22,10 +25,17 @@ export class TraumaticResultsIndicatorComponent implements OnInit {
   private traumaticSensitivityService = inject(TraumaticSensitivityService);
   @Input() scorePercentages: any;
   scoreKeys!: Array<keyof TestResult>;
+  readonly panelOpenState = signal(false);
+  sensitivityCategory!: Record<string, SensitivityCategory>;
+  panelOpenStates: { [key: string]: boolean } = {};
+
   ngOnInit(): void {
+    this.sensitivityCategory = sensitivityTest;
     this.scoreKeys = this.traumaticSensitivityService.getScoreKeys();
   }
-
+  togglePanel(key: string) {
+    this.panelOpenStates[key] = !this.panelOpenStates[key];
+  }
   getDescriptions(score: string): string {
     return this.traumaticSensitivityService.getResultsDescriptions(score);
   }
