65b2f24956a495f529d6560c8cec80e222edface
Ivan-Andrusyshyn <ivan.andrusyshyn@icloud.com>
2025-02-20 23:18:29 +0200

    add styles on tests card

--- src/app/content/tests-content/test-btn-data.ts
+++ src/app/content/tests-content/test-btn-data.ts
@@ -2,14 +2,14 @@ const testButtonData = Object.freeze([
   {
     title: 'Тест 16 типів особистості',
     description: 'Дізнайся більше про себе, свої сильні та слабкі сторони.',
-    testSvg: 'assets/svg/tests/icons-tests.svg#avatar-thinking-5',
+    testSvg: 'assets/svg/tests/16-personalities-card.svg',
     routeStart: '/tests/16-personalities/questions',
     routeInfo: '/tests/16-personalities/test-information',
   },
   {
     title: 'Тест на травматичну чуттєвість',
     description: 'Як сильно ти реагуєш на коментарі та зауваження?',
-    testSvg: 'assets/svg/tests/icons-tests.svg#avatar-thinking-1',
+    testSvg: 'assets/svg/tests/pick-heart.svg',
     routeStart: '/tests/traumatic-sensitivity/questions',
     routeInfo: '/tests/traumatic-sensitivity/test-information',
   },
