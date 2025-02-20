351c3a3264972eeb528cba90fed2744027369a9b
Ivan-Andrusyshyn <ivan.andrusyshyn@icloud.com>
2025-02-20 20:57:53 +0200

    change content on consultation components+ add styles btns

--- src/app/components/consultation/consultation.component.ts
+++ src/app/components/consultation/consultation.component.ts
@@ -1,9 +1,10 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-consultation',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './consultation.component.html',
   styleUrl: './consultation.component.scss',
 })
