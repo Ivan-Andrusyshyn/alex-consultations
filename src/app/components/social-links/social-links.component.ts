89a19d879cebf041bd3c3bacdc589f34b1117d0b
Ivan-Andrusyshyn <ivan.andrusyshyn@icloud.com>
2025-03-02 01:07:17 +0200

    fix bug with links color when change theme onsite

--- src/app/components/social-links/social-links.component.ts
+++ src/app/components/social-links/social-links.component.ts
@@ -1,12 +1,16 @@
+import { NgStyle } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-social-links',
   standalone: true,
-  imports: [],
+  imports: [NgStyle],
   templateUrl: './social-links.component.html',
   styleUrl: './social-links.component.scss',
 })
 export class SocialLinksComponent {
   @Input() titleLinks?: string = 'Приєднуйся';
+  @Input() isTheme: boolean = true;
+
+  textColor = 'var(--textColor)';
 }
