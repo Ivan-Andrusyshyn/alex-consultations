5538bff8aa738a213f0d5201a20cdd326c48e83c
Ivan-Andrusyshyn <ivan.andrusyshyn@icloud.com>
2025-03-18 16:06:21 +0200

    add

--- server/build/utils/cors.js
+++ server/build/utils/cors.js
@@ -2,10 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const corsOptions = {
     origin: [
+        'https://alex-consultations.vercel.app/',
         'http://localhost:4200',
         'https://vidchuttia.com.ua',
         'http://vidchuttia.com.ua',
-        'https://alex-consultations.vercel.app',
     ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
