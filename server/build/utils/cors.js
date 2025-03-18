1ae3e902a2a3bc72a56161638cf5ca5a913a9638
Ivan-Andrusyshyn <ivan.andrusyshyn@icloud.com>
2025-03-18 15:18:44 +0200

    add new logic in core on server

--- server/build/utils/cors.js
+++ server/build/utils/cors.js
@@ -1,10 +1,6 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const cors_1 = __importDefault(require("cors"));
-const corsOptions = () => (0, cors_1.default)({
+const corsOptions = {
     origin: [
         'http://localhost:4200',
         'https://vidchuttia.com.ua',
@@ -14,5 +10,5 @@ const corsOptions = () => (0, cors_1.default)({
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
-});
+};
 exports.default = corsOptions;
