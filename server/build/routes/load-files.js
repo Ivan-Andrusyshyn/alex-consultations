71e35ee5b84715336b7e0d76eec33b56d0f6a5d0
Ivan-Andrusyshyn <ivan.andrusyshyn@icloud.com>
2025-04-23 19:29:02 +0300

    delete loading all files on init

--- server/build/routes/load-files.js
+++ server/build/routes/load-files.js
@@ -4,7 +4,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const loadAllFiles_1 = __importDefault(require("../controllers/load-files/loadAllFiles"));
 const loadFilesRoute = (0, express_1.default)();
-loadFilesRoute.get('/load-files', loadAllFiles_1.default);
+// loadFilesRoute.get('/load-files', loadAllFiles);
 exports.default = loadFilesRoute;
