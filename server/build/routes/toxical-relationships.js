a016e8c1e127fd452999615df26ef1003d5c0b0c
Ivan-Andrusyshyn <ivan.andrusyshyn@icloud.com>
2025-07-03 13:34:12 +0300

    chang build on server

--- server/build/routes/toxical-relationships.js
+++ server/build/routes/toxical-relationships.js
@@ -4,10 +4,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const getNameCategoryByScore_1 = __importDefault(require("../controllers/toxical-relationship/getNameCategoryByScore"));
-const getQuestions_1 = __importDefault(require("../controllers/toxical-relationship/getQuestions"));
-const getDetailsCategory_1 = __importDefault(require("../controllers/toxical-relationship/getDetailsCategory"));
-const getTestInformation_1 = __importDefault(require("../controllers/toxical-relationship/getTestInformation"));
+const getNameCategoryByScore_1 = __importDefault(require("../controllers/toxical-relationships/getNameCategoryByScore"));
+const getQuestions_1 = __importDefault(require("../controllers/toxical-relationships/getQuestions"));
+const getDetailsCategory_1 = __importDefault(require("../controllers/toxical-relationships/getDetailsCategory"));
+const getTestInformation_1 = __importDefault(require("../controllers/toxical-relationships/getTestInformation"));
 const toxical_relationship_1 = require("../validators/toxical-relationship");
 const relationshipRoute = (0, express_1.default)();
 relationshipRoute.get('/toxical-relationships', getQuestions_1.default);
